19c7ac2659e3d0b131754029d7657eaf90713df5
luizbg <luizbarros8@gmail.com>
2017-09-10 23:47:55 -0300

    teste

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,6 +77,6 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       #params.require(:user).permit(:username, :password)
-      params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+      user_params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
     end
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :username, :password, :first_name, :last_name, :email,:created_at, :updated_at
+json.extract! user, :id, :username, :password, :first_name, :last_name, :email, :created_at, :updated_at
 json.url user_url(user, format: :json)
