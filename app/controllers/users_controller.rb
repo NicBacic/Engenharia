4c78658ad7ffdd0aca382d2eeb9bb5d22375fa65
luizbg <luizbarros8@gmail.com>
2017-09-10 23:59:13 -0300

    linhas bugadas comentadasO

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,7 +76,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      #params.require(:user).permit(:username, :password)
-      user_params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+      params.require(:user).permit(:username, :password)
+      #user_params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
     end
 end
