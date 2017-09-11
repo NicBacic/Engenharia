13b835e60a71500bdcfe7806685df6dc5828995a
Eduardo <dodaldo50@hotmail.com>
2017-09-11 17:13:51 -0300

    Returning to previous configuration

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = User.new(username: "Example User", password: "Example Pass")
-  end
-
-  test "should be valid" do
-    assert @user.valid?
-  end
-end
+  # test "the truth" do
+  #   assert true
+  # end
