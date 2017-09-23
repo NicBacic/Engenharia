71ed0dabd47e986479a865f57486ebcd5a7f1645
lumiturtle <lucasdesapereira@gmail.com>
2017-09-23 14:50:21 -0300

    Criar a pagina about, a melhor pagina ate o momento

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :users
   root 'users#index'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "get home" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
+  test "get help" do
     get static_pages_help_url
     assert_response :success
   end
 
+  test "get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
