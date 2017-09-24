91955eb0974a1efe1f8c4255fd2158826a76942d
NickBacic <nicbacic@gmail.com>
2017-09-24 12:16:15 -0300

    Página de Pesquisa de itens

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,6 @@ ActiveRecord::Schema.define(version: 20170905204154) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    #t.string "first_name"
-    #t.string "last_name"
-    #t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
