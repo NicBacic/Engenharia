692324ce98f399fc37fa6aa8b6b42965c6a28efb
giovanna.giorgi <giovanna.giorgi@yahoo.com.br>
2017-09-24 17:11:48 -0400

    Criar model item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   get 'static_pages/home'
 
   get 'static_pages/help'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905204154) do
+ActiveRecord::Schema.define(version: 20170924210512) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "nome"
+    t.string "descricao"
+    t.decimal "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    #t.string "first_name"
-    #t.string "last_name"
-    #t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
