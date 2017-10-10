9d75406fc23222445483639ef58d9c694b7e54c9
luizbg <luizbarros8@gmail.com>
2017-10-09 22:07:10 -0300

    Travis

--- test/system/reviews_test.rb
+++ test/system/reviews_test.rb
@@ -4,6 +4,6 @@ class ReviewsTest < ApplicationSystemTestCase
   # test "visiting the index" do
   #   visit reviews_url
   #
-  #   assert_selector "h1", text: "Review"
+  #   assert_selector "h1", text: "Reviews"
   # end
 end
