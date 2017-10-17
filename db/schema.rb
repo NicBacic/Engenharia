e039ac9f410a529a4dcdf38bee55f60e7c30eec1
NickBacic <nicbacic@gmail.com>
2017-10-17 20:52:29 -0200

    Arrumando testes avaliar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009202920) do
+ActiveRecord::Schema.define(version: 20171017220635) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "nome"
+    t.string "descricao"
+    t.decimal "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jogos", force: :cascade do |t|
     t.string "nome"
