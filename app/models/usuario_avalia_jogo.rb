084db17c42c760b69a2d41b0aa5294293c500b43
NickBacic <nicbacic@gmail.com>
2017-10-20 23:27:07 -0200

    Mudança na model para respeitar chave estrangeira

--- app/models/usuario_avalia_jogo.rb
+++ app/models/usuario_avalia_jogo.rb
@@ -1,4 +1,5 @@
 class UsuarioAvaliaJogo < ApplicationRecord
-  belongs_to :user
-  belongs_to :jogo
+  belongs_to :user, class_name: 'User', foreign_key: 'id_user'
+  
+  belongs_to :jogo, class_name: 'Jogo', foreign_key: 'id_jogo'
 end
