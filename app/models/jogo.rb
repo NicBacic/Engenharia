41b74f1831b2b601502766dbb6cd436c8b6b4a57
lumiturtle <lucasdesapereira@gmail.com>
2017-11-04 03:40:37 -0200

    Foda-se

--- app/models/jogo.rb
+++ app/models/jogo.rb
@@ -6,16 +6,19 @@ class Jogo < ApplicationRecord
   validates :rating, numericality: {greater_than: -1, less_than_or_equal_to: 10}, allow_blank: true 
   validates :nome, presence: true
   validates :desenvolvedora, presence: true
-  validates :console, presence: true
+  #validates :console, presence: true
 
   has_many :usuarioAvaliaJogos
   has_many :reviews
+
   def self.search(term)
     if term
       where('lower(nome) LIKE ?', "%#{term}%".downcase)
       #order('nome ASC')
     else
       order('nome ASC')
+
+
     end
   end
 
