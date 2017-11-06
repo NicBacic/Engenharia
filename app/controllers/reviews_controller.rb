21d028879a9bd76864b06258526b9f08ee686042
Luiz Barros Galdino <luizbarros8@gmail.com>
2017-11-05 23:18:36 -0200

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,39 +4,25 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
     @review = Review.new(review_params)
-
     if current_user.present?
-      @review = Review.new(review_params_user)
-      @user = current_user
-      @user.reviews.create(params[review_params])
-    else
-      @review = Review.new(review_params)
+      @user = User.find(params[:user_id])
+      @review = @user.reviews.create(params[review_params])
     end
 
     if @review.save
       flash[:success] = "Review created!"
-      flash[:success] = 'Review created!'
-      @jogo = @review.jogo
-      redirect_to @jogo
     else
       flash[:error] = "Review wasn't saved"
-      redirect_to @jogo
     end
-    
+    @jogo = @review.jogo
+    redirect_to @jogo
   end
 
   private
 
   def review_params
     params.require(:review).permit(:texto).merge(jogo_id: current_jogos.id)
-  end
-
-  private
-
-  def review_params
-    params.require(:review).permit(:texto).merge(:jogo_id => current_jogos.id)
   end
 
   private
