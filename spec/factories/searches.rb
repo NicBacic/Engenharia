9af4dd5864b7a4383f59e88cec497b49b42a81f9
lumiturtle <lucasdesapereira@gmail.com>
2017-11-16 21:50:28 -0200

    Busca avançada

---
+++ spec/factories/searches.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :search do
+    nome "MyString"
+    console "MyString"
+    publisher "MyString"
+    desenvolvedora "MyString"
+    min_rating "9.99"
+    max_rating "9.99"
+  end
+end
---
+++ test/controllers/searches_controller_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class SearchesControllerTest < ActionDispatch::IntegrationTest
+
+end
