5de61becbd7c9653fad447ce2eb12c8b08f4fd05
lumiturtle <lucasdesapereira@gmail.com>
2017-12-03 22:24:07 -0200

    Test

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -29,4 +29,19 @@ let(:valid_attributes) {
   
   let(:valid_session) { {} }
 
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new, params: {}, session: valid_session
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      user = Search.create! valid_attributes
+      get :show, params: {id: search.to_param}, session: valid_session
+      expect(response).to be_success
+    end
+  end
+
 end
