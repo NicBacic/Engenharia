83cf248fc8323c555cef8d2ca0eb1a1c78e25a45
lumiturtle <lucasdesapereira@gmail.com>
2017-12-03 22:42:26 -0200

    test

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is valid with valid attributes" do
+    expect(FactoryBot.create :search).to be_valid
+  end
+
 end
