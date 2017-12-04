6ecda7f821cf8d6618b00b06e853c92cc61d54ce
lumiturtle <lucasdesapereira@gmail.com>
2017-12-03 22:27:10 -0200

    Test

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -4,44 +4,4 @@ Coveralls.wear!
 
 RSpec.describe SearchesController, type: :controller do
 
-let(:valid_attributes) {
-    {
-      nome: "The Legend of Zelda"
-      console: "Switch"
-      publisher: "Nintendo"
-      desenvolvedora: "Nintendo EPD"
-      min_rating: "0"
-      max_rating: "9.99"  
-    }
-  }
-
-  let(:invalid_attributes) {
-    {
-      nome: "*****",
-      console: "*****",
-      publisher:"??????",
-      desenvolvedora:"??????",
-      in_rating: "-2"
-      max_rating: "-1"  
-    }
-  }
-
-  
-  let(:valid_session) { {} }
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      user = Search.create! valid_attributes
-      get :show, params: {id: search.to_param}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
 end
